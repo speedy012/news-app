24a756e5de7030fe6b9b9e3ba9a51a371d41fb75
Herman Rivas <hrivas5726@aol.com>
2020-08-18 11:59:25 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState('')
+
+  // useEffect(() =>
+  // fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=9152561a7d9f477eabb7741f4a904434")
+  //   .then(res => res.json())
+  //   .then(res => console.log(res))
+  // );
+
+  let handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(search)
+
+    fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=`)
+    //   .then(res => res.json())
+    //   .then(res => console.log(res))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2 className="title"> <strong>Newsorama</strong></h2>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name: <br/>
+        <input type="text" value={search} onChange={e => setSearch(e.target.value)}/>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
     </div>
   );
 }
