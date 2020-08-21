984905f4047c6724071b2d6cf8612ec473a4aca5
Herman Rivas <hrivas5726@aol.com>
2020-08-21 15:25:59 -0400

    cors

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ const App = () => {
 
   const fetchData = (searchWord) => {
     setIsLoading(true)
-    fetch(`https://newsapi.org/v2/everything?q=${searchWord}&apiKey=9152561a7d9f477eabb7741f4a904434`)
+    let url = 'https://newsapi.org/v2/everything?q=${searchWord}&apiKey=9152561a7d9f477eabb7741f4a904434'
+
+    fetch(`https://cors-anywhere.herokuapp.com/${url}`)
      .then(res => res.json())
      .then(res => setArticles(res.articles))
      .catch(err => console.error(err))
