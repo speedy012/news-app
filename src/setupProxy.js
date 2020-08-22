a195a19ed24d84325968c98d7f68e343d678c22b
Herman Rivas <hrivas5726@aol.com>
2020-08-21 20:07:53 -0400

    gdFeeling

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/v2/everything',
+    '/v2',
     createProxyMiddleware({
        target: 'https://newsapi.org',
        changeOrigin: true
